0851447728384a8e2f6c6ac5bde07bca1e8d0d25
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-04-17 06:24:21 +0100

    Update Alert.jsx

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -6,7 +6,7 @@ const Alert = ({title, para}) => {
     <div className="bg-[#140926] max-sm:w-full max-sm:h-auto w-[710px] h-[105px] rounded-[16px] p-[20px] flex justify-center items-center gap-[11px] flex-col">
        <div className="font-[700] text-center">{title}</div>
        <div className="w-full"><hr className="w-full text-[#F1F1F11A]"/></div>
-       <div className="text-[#8C8C8C] font-[300] text-[12px] text-center">{para}</div>
+       <div className="text-[#8C8C8C] font-[300] text-[14px] text-center">{para}</div>
     </div>
   )
 }
