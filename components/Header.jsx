a20d5b1e28ba2e1ead98b298388d607e45ae89b3
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-04-17 06:26:58 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className='flex flex-col z-1 relative pb-40 transition-all duration-300 ease-in-out'>
         <div className='flex justify-center items-center relative py-10'>
-            <span onClick={()=> router.back()} className='absolute text-[#8C8C8C] font-[500] text-[12px] left-10 max-sm:top-5 cursor-pointer hover:text-white'>Back</span>
+            <span onClick={()=> router.back()} className='absolute text-[#8C8C8C] font-[500] text-[14px] left-10 max-sm:top-5 cursor-pointer hover:text-white'>Back</span>
             <div className='flex flex-col items-center'>
             <h1 className='text-[32px] font-[600]'>Choose Options</h1>
             <p className='text-[#8C8C8C] text-[14px] font-[400]'>Choose your settings, we'll handle the editing</p>
